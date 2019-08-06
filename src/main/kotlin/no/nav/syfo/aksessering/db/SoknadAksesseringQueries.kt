fc4377b156bbf80937d5106f67a68dd70b58ebdb
Gøran Berntsen <goran.berntsen@nav.no>
2019-08-06 14:59:34 +0200

    Added tests for postgres

---
+++ src/main/kotlin/no/nav/syfo/aksessering/db/SoknadAksesseringQueries.kt
@@ -0,0 +1,27 @@
+package no.nav.syfo.aksessering.db
+
+import no.nav.syfo.db.DatabaseInterface
+import no.nav.syfo.objectMapper
+import no.nav.syfo.persistering.SoknadRecord
+import java.sql.ResultSet
+
+fun DatabaseInterface.hentSoknad(SoknadId: String): SoknadRecord =
+        connection.use { connection ->
+            connection.prepareStatement(
+                    """
+                        SELECT soknad_id, innsendt_dato, soknad 
+                        FROM soknader
+                        WHERE soknad_id=?;
+                    """.trimIndent()
+            ).use {
+                it.setString(1,SoknadId)
+                it.executeQuery().toSoknadRecord()
+            }
+        }
+
+fun ResultSet.toSoknadRecord(): SoknadRecord =
+        SoknadRecord(
+                soknadId = getString("soknad_id"),
+                innsendtDato = getDate("innsendt_dato"),
+                soknad = getString("soknad")
+        )
